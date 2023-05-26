47d4bb3f0a6fe07382ea7fa71ea65676787e9fe4
Felixyu007 <cen981125@gmail.com>
2023-05-26 14:37:23 -0700

    updated question 1

--- src/week1/Question1/Felix.java
+++ src/week1/Question1/Felix.java
@@ -2,6 +2,13 @@ package week1.Question1;
 
 public class Felix {
 
-    // felix
+    public static String method1(int num){
+
+        if ((num % 2)==0){
+            return "even";
+        }else {
+            return "odd";
+        }
+    }
 
 }
