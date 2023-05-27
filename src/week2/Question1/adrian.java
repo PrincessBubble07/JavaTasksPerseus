0b691345e216664e2e7e05a789292dc7421cefce
Adrian <adrianbrinzila@yahoo.com>
2023-05-27 16:06:10 -0700

    testing git

---
+++ src/week2/Question1/adrian.java
@@ -0,0 +1,13 @@
+package week2.Question1;
+
+public class adrian {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int y = 30;
+
+        x = x + y;
+
+        System.out.println(" x = " + x + "\n y = " + y);
+    }
+}
