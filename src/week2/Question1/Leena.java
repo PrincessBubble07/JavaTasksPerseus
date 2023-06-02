81209b1beff52a49e67ea43bc42b37716094c766
leena <leena@cybertekschool.com>
2023-06-02 22:17:28 +0300

    sample solutions are added

--- src/week2/Question1/Leena.java
+++ src/week2/Question1/Leena.java
@@ -7,4 +7,12 @@ public class Leena {
 
     Swap two variable' values without using a third variable
      */
+       public static void swapNumbers(int num1, int num2){
+
+           num1 = num1 + num2;
+           num2 = num1 - num2;
+           num1 = num1 - num2;
+           System.out.println("Swapped num1 = "+num1);
+           System.out.println("Swapped num2 = " + num2);
+       }
 }
--- src/week2/Question3/Leena.java
+++ src/week2/Question3/Leena.java
@@ -27,4 +27,25 @@ public class Leena {
             17
             ....
    */
+      public static void printConsecutiveNumbers(int N) {
+          for (int i = 1; i <= N; i++) {
+              String output = "";
+
+              if (i % 2 == 0) {
+                  output += "Codility";
+              }
+              if (i % 3 == 0) {
+                  output += "Test";
+              }
+              if (i % 5 == 0) {
+                  output += "Coders";
+              }
+
+              if (output.isEmpty()) {
+                  System.out.println(i);
+              } else {
+                  System.out.println(output);
+              }
+          }
+      }
 }
