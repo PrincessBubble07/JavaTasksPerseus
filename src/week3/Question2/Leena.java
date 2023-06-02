0b74536814015dcde5d2f5c329f4af92152d19c4
leena <leena@cybertekschool.com>
2023-06-03 01:00:46 +0300

    Week3 questions are added

---
+++ src/week3/Question2/Leena.java
@@ -0,0 +1,9 @@
+package week3.Question2;
+
+public class Leena {
+       /*Numbers -- Reverse Negative Number
+    Write a return method that can reverse digits of a negative number and return it as int
+    input: -123  output: -321
+
+     */
+}
