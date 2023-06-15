f758cf0411c4c138ba77a1478aed2aee0e0c479e
Felixyu007 <cen981125@gmail.com>
2023-06-14 23:21:43 -0700

    week 4 solutions

---
+++ src/week4/Question2/Felix.java
@@ -0,0 +1,36 @@
+package week4.Question2;
+
+public class Felix {
+
+    public static boolean method2(String str1,String str2){
+
+        boolean answer = true;
+
+        if (str1.length()!=str2.length())
+            answer=false;
+
+        for (int i = 0; i < str1.length(); i++) {
+
+            if (str1.contains(""+str2.charAt(i))){
+                answer=true;
+            }else {
+                answer=false;
+            }
+
+        }
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(method2("abc", "bca"));
+    }
+
+}
+/*
+    String -- Same letters
+Write a return method that check if a string is build out of the
+same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+     */
