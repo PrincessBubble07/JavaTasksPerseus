ec85bd0d7828c71f4869cf3c0799d166b23b542c
leena <leena@cybertekschool.com>
2023-06-19 00:51:38 +0300

    week4 solution added

--- src/week4/Question1/Leena.java
+++ src/week4/Question1/Leena.java
@@ -7,4 +7,16 @@ Write a return method that can find the frequency of
 characters
 Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
  */
+    public static void main(String[] args) {
+
+    }
+
+    public static void sleep() throws InterruptedException {
+
+
+            Thread.sleep(2000); //checked
+
+        System.out.println(10/0); //unchecked compile is fine runtime exceptions
+
+    }
 }
