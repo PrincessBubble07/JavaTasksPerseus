f89a77a3b970510cc7543a001109caea635fe072
leena <leena@cybertekschool.com>
2023-06-28 00:26:10 +0300

    week6 tasks are added

---
+++ src/week6/Question3/leena.java
@@ -0,0 +1,11 @@
+package week6.Question3;
+
+public class leena {
+    /*
+    Sum of digits in a string:
+  Write a method that can return the sum of the digits in a string.
+
+  Ex: "A3v15" = > 3 + 1 + 5 = 9
+        return 9
+     */
+}
