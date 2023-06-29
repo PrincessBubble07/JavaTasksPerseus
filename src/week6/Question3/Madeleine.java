78ad3f01f3d78b8cc74f93def01c30cbd14ec678
MadeleineCydeo <consciousbubbling@gmail.com>
2023-06-28 22:35:50 -0700

    added week 6 solutions

---
+++ src/week6/Question3/Madeleine.java
@@ -0,0 +1,34 @@
+package week6.Question3;
+
+public class Madeleine {
+
+    public static int sumOfDigits(String str){
+
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                sum += Character.getNumericValue(ch);
+
+            }
+        }
+        return sum;
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+}
