c556e6d79971a59b3dcce1e6bc04483a2e54a36d
MadeleineCydeo <consciousbubbling@gmail.com>
2023-06-28 19:43:38 -0700

    added week 5 solutions

--- src/week5/Question1/Madeleine.java
+++ src/week5/Question1/Madeleine.java
@@ -1,4 +1,30 @@
 package week5.Question1;
 
 public class Madeleine {
+
+    public static String uniqueChars(String str) {
+
+        String unique = "";
+
+        for (int j = 0; j < str.length(); j++) {     //iterate over each character in str
+
+            char ch = str.charAt(j);         // char variable ch to represent the chars on the string at each index
+            int frequency = 0;       //int variable to help determine if ch matches another char on str
+
+
+            for (int i = 0; i < str.length(); i++) {    //checks how many times the ch has appeared in the string str
+                if (str.charAt(i) == ch) {      //if the ch has appeared in the string
+                    frequency++;    //increase frequency by 1
+                }
+            }
+
+            if (frequency == 1) {   // if the frequency is equal to one it is unique
+                unique += ch;
+            }
+        }
+
+        return unique;
+
+    }
+
 }
--- src/week5/Question2/Madeleine.java
+++ src/week5/Question2/Madeleine.java
@@ -1,4 +1,22 @@
 package week5.Question2;
 
 public class Madeleine {
+    public static String reverse(String str) {
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(reverse("noopS nedooW"));
+
+
+    }
 }
