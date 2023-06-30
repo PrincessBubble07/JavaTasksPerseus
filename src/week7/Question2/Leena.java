15f7d7e9d8180eb1b21ac6aff009ebd547a92a74
leena <leena@cybertekschool.com>
2023-06-30 18:43:05 +0300

    week7 tasks added

---
+++ src/week7/Question2/Leena.java
@@ -0,0 +1,9 @@
+package week7.Question2;
+
+public class Leena {
+    /*
+    Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
+Ex: int[] arr = {10, 9, 8, 7};
+arr = Sort(arr); ==>{ 7, 8, 9, 10};
+     */
+}
