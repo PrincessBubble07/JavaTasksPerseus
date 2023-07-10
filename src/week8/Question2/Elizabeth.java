22bfc36cc4d98e4db7e1b0876e3fa6c4dfd48f86
yelyzaveta94 <suhostavets.e@gmail.com>
2023-07-10 10:02:39 -0400

    solution added

---
+++ src/week8/Question2/Elizabeth.java
@@ -0,0 +1,43 @@
+package week8.Question2;
+
+import java.util.Arrays;
+
+public class Elizabeth {
+
+    /*
+Array - Move Zeros to the End
+Write a method that can move all the zeros to last indexes of the array (Do
+Not Use Sort Method) Ex: input: {1,0,2,0,3,0,4,0}; output: [1, 2, 3, 4, 0, 0, 0,
+0]
+     */
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(moveZeros(1, 0,2, 0, 3, 0, 4, 0)));
+    }
+    public static int[] moveZeros(int... nums){
+
+        int[] moved = new int[nums.length];
+        int index = 0; // 1
+
+        for(int each : nums){ //  [5, 2, 1, 23, 0, 0, 0, 0]
+            if(each != 0){
+                moved[index++] = each;
+            }
+        }
+
+        return moved;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
