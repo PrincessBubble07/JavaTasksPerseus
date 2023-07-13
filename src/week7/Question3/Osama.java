dcbd068508f66341681827e1957e3b6793507da6
osama_aziz <osamaaziz89@gmail.com>
2023-07-12 22:54:03 -0400

    Week 8 added

--- src/week7/Question3/Osama.java
+++ src/week7/Question3/Osama.java
@@ -12,7 +12,7 @@ public class Osama {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] < arr[j + 1]) {
-                    int swappable = arr[j];
+                   int swappable = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = swappable;
                 }
