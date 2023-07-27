6069c0b76613faa7599534b5bd56403b5e70d80f
osama_aziz <osamaaziz89@gmail.com>
2023-07-26 21:17:54 -0400

    week 9 added

--- src/week9/Question1/Leena.java
+++ src/week9/Question1/Leena.java
@@ -3,6 +3,6 @@ package week9.Question1;
 public class Leena {
     /*
     Array - Concat two arrays
-  Write a return method that can concate two arrays.
+  Write a return method that can concat two arrays.
      */
 }
