8d9e8331144d21ef4fd20e309da2ffa07a7cc3b0
leena <leena@cybertekschool.com>
2023-07-28 23:28:50 +0300

    week9 solutions added

--- src/week9/Question3/Leena.java
+++ src/week9/Question3/Leena.java
@@ -1,8 +1,20 @@
 package week9.Question3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Leena {
     /*
     ArrayList - Remove some values
   Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
      */
+    public static void main(String[] args) {
+        ArrayList<Integer> names = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,101,200,300));
+        removeValuesGreaterThan100(names);
+    }
+
+    public static void removeValuesGreaterThan100(List<Integer> list) {
+        list.removeIf(num -> num > 100);
+    }
 }
